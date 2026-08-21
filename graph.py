99e41ea17781671d5909e7fe1410c8a4f18324eb
JhonatandeAlmeida <99823517@ambev.com.br>
2026-08-21 12:00:55 -0300

    Refactor graph.py to improve structure and readability

--- graph.py
+++ graph.py
@@ -193,6 +193,15 @@ def make_graph_repasse(df: pd.DataFrame, h_chart, w_chart, h_pic, w_pic, canal):
 #depara_repasse['Caminho'] = depara_repasse['Caminho'].apply(convert_image)
 #depara_repasse.to_parquet('data/repasse/depara_repasse.parquet')
 
+if check_for_new_file(
+    "data/repasse/graficos.xlsx",
+    "data/repasse/*.parquet"
+):
+    read_excel_parquets(
+        "data/repasse/graficos.xlsx",
+        "data/repasse"
+    )
+
 repasse = pl.read_parquet("data/repasse/repasse.parquet")
 
 depara_repasse = pl.read_parquet("data/repasse/depara_repasse.parquet")
